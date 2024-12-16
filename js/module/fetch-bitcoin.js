87b83174482d3552e94f3a0fb6c420f99a1d9a21
Bruno <fellipesouza945@gmail.com>
2024-12-16 19:20:50 -0300

    refatorar fetch bitcoin

--- js/module/fetch-bitcoin.js
+++ js/module/fetch-bitcoin.js
@@ -1,15 +1,15 @@
 
-export default function initFetchBitcoin(){
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target){
+  fetch(url)
 .then(response => response.json())
 .then(json => {
-  const btcBitcoin = document.querySelector('.btc-preco');
+  const btcBitcoin = document.querySelector(target);
   btcBitcoin.innerText = (100/json.BRL.sell).toFixed(4);
   
 }).catch(erro => {
   console.log(Error(erro));
 }
 )
-};
+}; 
 
 
